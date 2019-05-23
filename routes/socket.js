eb76f929c7444e8661a1114bfc187961874fd7ee
HyeongChang Lee <hc9904@naver.com>
2019-05-24 00:07:05 +0900

    Update socket test

--- routes/socket.js
+++ routes/socket.js
@@ -6,6 +6,17 @@ socketApi.io = io;
 
 io.on("connection", function(socket) {
   console.log("A user connected");
+
+  /* 채팅 메세지 */
+  socket.on("chat-push", function(msg) {
+    console.log("msg: " + msg);
+    io.emit("chat-pull", msg);
+  });
+
+  /* 연결끊김 */
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
 });
 
 socketApi.sendNotification = function() {
